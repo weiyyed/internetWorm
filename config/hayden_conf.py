68051af034a88a4fd9187145979ef31dedaa452b
weiyyed <weiyyed@163.com>
2019-12-06 20:34:03 +0800

    移动+pc=第一版本

--- config/hayden_conf.py
+++ config/hayden_conf.py
@@ -1,4 +1,5 @@
 # sy的域名
 DOMAIN="http://sy.51gxc.com"
+# DOMAIN="http://v3-test-linux-sy.hd-cloud.com"
 USERNAME="1031"
 PASSWORD="1"
--- hayden_permission_demo.py
+++ hayden_permission_demo.py
@@ -1,11 +1,10 @@
 # v3获取权限中菜单并保存到文件
-import datetime
-import json,re,time,requests
+import datetime,json,re,time,requests
+from json import JSONDecodeError
 from selenium import webdriver
 from urllib.parse import urlencode
 from config import hayden_conf
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -19,9 +18,14 @@ def get_html_source(url,session):
         else:
             raise Exception("请求失败{}".format(req.status_code))
 def parse_html(data):
-    data=json.loads(data)
-    if data and "data" in data.keys():
-        items=data.get("data")
+    # 解析权限页
+    data_change=None
+    try:
+        data_change=json.loads(data)
+    except JSONDecodeError:
+        print("数据不是json",data)
+    if data_change and "data" in data_change.keys():
+        items=data_change.get("data")
         merge={}
         for item in items:
             if item["funcode"]!=None:
@@ -44,12 +48,14 @@ def parse_html(data):
                         merge[item["codepath"]].append(item["name"])
                 else:
                     merge[item["codepath"]]=[item["name"]]
-        return merge
+        # return merge
     else:
         raise Exception("解析data数据失败")
+    order_dic = sorted(merge.items(), key=lambda x: x[0])
+    return order_dic
 #     登录并获取session
-def generate_session(url,name,password):
-    # try:
+def generate_session(url,name,password,module="sy"):
+    # module:访问的模块
     option=Options()
     option.add_argument("--headless")
     option.add_argument("--disable-gpu")
@@ -62,11 +68,14 @@ def generate_session(url,name,password):
     # 设置等待
     wait=WebDriverWait(driver,10)
     wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/script")))
+
+    if module=="sy":
+        driver.get(url+"#eyJtYyI6InN5IiwiZmMiOiJTWV9QRVJNSVNTT05fUEMiLCJ1YyI6IjAxNzAwMDQ1MDAxMCJ9")
     time.sleep(1)
-    cookies=driver.get_cookies()
     html_source=driver.page_source
-    # print(html_source)
     csrf=re.search("window.csrf = '(.*?)'",html_source).group(1)
+    cookies=driver.get_cookies()
+    # print(html_source)
     driver.close()
     # 设置requests的session
     headers = {
@@ -81,30 +90,32 @@ def generate_session(url,name,password):
         sess.cookies.set(cookie["name"],cookie["value"])
     sess.headers.update({"csrf":csrf})
     return sess
-def main():
-    domain=hayden_conf.DOMAIN
-    url=domain+"/sy/SY_PERMISSON_PC/getPermissionTree?"
-    params={
-        "clientType": 0,
-        "contentType": "json",
-        "ajax": "true",
-    }
-    url=url+urlencode(params)
-    # 用户密码
-    user=hayden_conf.USERNAME
-    pwd=hayden_conf.PASSWORD
-    req_session=generate_session(domain,user,pwd)
-    source=get_html_source(url,req_session)
-    order_dic=sorted(parse_html(source).items(),key=lambda x:x[0])
-    with open("file/{}_{}_{}.txt".format(domain[7:],user,datetime.date.today()),"w",encoding="utf-8") as f:
-        # for k,v in order_dic.items():
-        #     f.write(k+":"+str(v)+"\n")
-        #     print(k,v)
-        for d in order_dic:
+def save_to_file(parse_data_dic,menu="pc"):
+    with open("file/{0}_{1}_{2}_{3}.txt".format(menu,hayden_conf.DOMAIN[7:].split("/")[0].split(":")[0],hayden_conf.USERNAME,datetime.date.today()),"w",encoding="utf-8") as f:
+        for d in parse_data_dic:
             k,v=d
             k1=str(k).split("#",1)[-1]
             if len(str(k).split("#",1))==1:
                 f.write("\n")
             f.write(k1 + ":" + str(v) + "\n")
-
-main()
+def main():
+    # url=hayden_conf.DOMAIN+"/sy/SY_PERMISSON_PC/getPermissionTree?"
+    params={
+        "clientType": 0,
+        "contentType": "json",
+        "ajax": "true",
+    }
+    url=hayden_conf.DOMAIN+"/sy/SY_PERMISSON_PC/getPermissionTree?"+urlencode(params)
+    # 用户密码
+    user=hayden_conf.USERNAME
+    pwd=hayden_conf.PASSWORD
+    req_session=generate_session(hayden_conf.DOMAIN,user,pwd)
+    # PC端
+    source=get_html_source(url,req_session)
+    save_to_file(parse_html(source),menu="pc")
+# 移动端
+    url_mobile = hayden_conf.DOMAIN + "/sy/SY_PERMISSION_M/getPermissionTree?"+ urlencode(params)
+    source_mobile = get_html_source(url_mobile, req_session)
+    save_to_file(parse_html(source_mobile),menu="m")
+if __name__ == '__main__':
+    main()
